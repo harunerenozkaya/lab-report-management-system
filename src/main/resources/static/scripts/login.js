450543eaf3e5e73658ac0f25ff96c0bc35b1bf19
Harun <harunerenozkaya@gmail.com>
2022-05-31 23:55:28 +0300

    boostrap has been added and pages has been customized

--- src/main/resources/static/scripts/login.js
+++ src/main/resources/static/scripts/login.js
@@ -1,5 +1,4 @@
 //This function redirects to login controller and control whether login inputs are correct
 function login() {
-    alert("geldi");
-    location.href = 'login/' + document.getElementById("id").value + '/' + document.getElementById("pwd").value;
+    location.href = '/login/' + document.getElementById("id").value + '/' + document.getElementById("pwd").value;
 }
